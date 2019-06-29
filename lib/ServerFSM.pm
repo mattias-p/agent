6e6353e2150ebdb91d8ce4d1e004a52d69e04ce7
Mattias Päivärinta <mattias@paivarinta.se>
2019-06-29 23:27:18 +0200

    Refactor

--- lib/ServerFSM.pm
+++ lib/ServerFSM.pm
@@ -6,7 +6,7 @@ use Exporter qw( import );
 use FSM::Builder;
 use Readonly;
 
-our @EXPORT_OK = qw( new_server_fsm %INPUT_PRIORITIES $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_USR1 $I_ALRM $I_HUP $I_TERM $I_EXIT );
+our @EXPORT_OK = qw( new_server_fsm cmp_inputs %INPUT_PRIORITIES $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_USR1 $I_ALRM $I_HUP $I_TERM $I_EXIT );
 
 Readonly our $S_LOAD        => 'LOAD';
 Readonly our $S_RUN         => 'RUN';
@@ -163,6 +163,11 @@ $BUILDER->define_input(
 
 sub new_server_fsm {
     return $BUILDER->build( initial_state => $S_LOAD );
+}
+
+sub cmp_inputs {
+    my ( $a, $b ) = @_;
+    $INPUT_PRIORITIES{ $a } <=> $INPUT_PRIORITIES{ $b }
 }
 
 1;
--- server.pl
+++ server.pl
@@ -6,11 +6,8 @@ use feature 'say';
 use Readonly;
 use Heap::Binary;
 use POSIX qw( pause );
-use ServerFSM qw( new_server_fsm %INPUT_PRIORITIES $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_ALRM $I_USR1 $I_HUP $I_TERM $I_EXIT );
+use ServerFSM qw( new_server_fsm cmp_inputs $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_ALRM $I_USR1 $I_HUP $I_TERM $I_EXIT );
 use Signal qw( install_handler retrieve_caught );
-
-my $input_queue =
-  Heap::Binary->new( sub { $INPUT_PRIORITIES{ $_[0] } <=> $INPUT_PRIORITIES{ $_[1] } } );
 
 my %actions = (
     $S_LOAD => sub {
@@ -59,18 +56,19 @@ install_handler( 'HUP' );
 install_handler( 'TERM' );
 install_handler( 'USR1' );
 
-my $fsm = new_server_fsm();
+my $event_stream = Heap::Binary->new( \&cmp_inputs );
+my $fsm          = new_server_fsm();
 while ( $fsm->current ne $S_EXIT ) {
     my @events = $actions{ $fsm->current }->();
 
-    $input_queue->insert($_) for @events;
-    $input_queue->insert($I_ALRM) if retrieve_caught('ALRM');
-    $input_queue->insert($I_CHLD) if retrieve_caught('CHLD');
-    $input_queue->insert($I_HUP)  if retrieve_caught('HUP');
-    $input_queue->insert($I_TERM) if retrieve_caught('TERM');
-    $input_queue->insert($I_USR1) if retrieve_caught('USR1');
+    $event_stream->insert($_) for @events;
+    $event_stream->insert($I_ALRM) if retrieve_caught('ALRM');
+    $event_stream->insert($I_CHLD) if retrieve_caught('CHLD');
+    $event_stream->insert($I_HUP)  if retrieve_caught('HUP');
+    $event_stream->insert($I_TERM) if retrieve_caught('TERM');
+    $event_stream->insert($I_USR1) if retrieve_caught('USR1');
 
-    my $input = $input_queue->extract_min() // $I_ZERO;
+    my $input = $event_stream->extract_min() // $I_ZERO;
 
     $fsm->process($input);
 }
