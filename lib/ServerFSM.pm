7a0b8ce1e36b4d4ee1df149ac30a01f9a08603fa
Mattias Päivärinta <mattias@paivarinta.se>
2019-06-29 22:56:34 +0200

    More states

--- lib/ServerFSM.pm
+++ lib/ServerFSM.pm
@@ -6,75 +6,143 @@ use Exporter qw( import );
 use FSM::Builder;
 use Readonly;
 
-our @EXPORT_OK = qw( new_server_fsm $S_LOAD $S_RUN $S_SLEEP $S_GRACE $S_SHUTDOWN $S_EXIT $I_DEFAULT $I_ALRM $I_HUP $I_TERM $I_EXIT );
+our @EXPORT_OK = qw( new_server_fsm %INPUT_PRIORITIES $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_USR1 $I_ALRM $I_HUP $I_TERM $I_EXIT );
 
-Readonly our $S_LOAD     => 'LOAD';
-Readonly our $S_RUN      => 'RUN';
-Readonly our $S_SLEEP    => 'SLEEP';
-Readonly our $S_GRACE    => 'GRACE';
-Readonly our $S_SHUTDOWN => 'SHUTDOWN';
-Readonly our $S_EXIT     => 'EXIT';
+Readonly our $S_LOAD        => 'LOAD';
+Readonly our $S_RUN         => 'RUN';
+Readonly our $S_SLEEP       => 'SLEEP';
+Readonly our $S_REAP        => 'REAP';
+Readonly our $S_WATCH       => 'WATCH';
+Readonly our $S_SLEEP_GRACE => 'SLEEP_GRACE';
+Readonly our $S_REAP_GRACE  => 'REAP_GRACE';
+Readonly our $S_WATCH_GRACE => 'WATCH_GRACE';
+Readonly our $S_SHUTDOWN    => 'SHUTDOWN';
+Readonly our $S_EXIT        => 'EXIT';
 
-Readonly our $I_DEFAULT => 'default';
-Readonly our $I_ALRM    => 'alrm';
-Readonly our $I_HUP     => 'hup';
-Readonly our $I_TERM    => 'term';
-Readonly our $I_EXIT    => 'exit';
+Readonly our $I_ZERO => 'zero';
+Readonly our $I_DONE => 'done';
+Readonly our $I_CHLD => 'chld';
+Readonly our $I_USR1 => 'usr1';
+Readonly our $I_ALRM => 'alrm';
+Readonly our $I_HUP  => 'hup';
+Readonly our $I_TERM => 'term';
+Readonly our $I_EXIT => 'exit';
+
+Readonly our %INPUT_PRIORITIES => (
+    $I_DONE => 0,
+    $I_EXIT => 1,
+    $I_TERM => 2,
+    $I_CHLD => 3,
+    $I_HUP  => 4,
+    $I_ALRM => 5,
+    $I_USR1 => 6,
+    $I_ZERO => 7,
+);
 
 Readonly my $BUILDER => FSM::Builder->new();
 
 $BUILDER->define_input(
-    $I_DEFAULT => (
-        $S_LOAD     => $S_RUN,
-        $S_RUN      => $S_SLEEP,
-        $S_SLEEP    => $S_SLEEP,
-        $S_GRACE    => $S_GRACE,
-        $S_SHUTDOWN => $S_EXIT,
-        $S_EXIT     => $S_EXIT,
+    $I_ZERO => (
+        $S_LOAD        => $S_RUN,
+        $S_RUN         => $S_RUN,
+        $S_REAP        => $S_SLEEP,
+        $S_WATCH       => $S_SLEEP,
+        $S_SLEEP       => $S_SLEEP,
+        $S_REAP_GRACE  => $S_SLEEP_GRACE,
+        $S_WATCH_GRACE => $S_SLEEP_GRACE,
+        $S_SLEEP_GRACE => $S_SLEEP_GRACE,
+        $S_SHUTDOWN    => $S_SHUTDOWN,
+        $S_EXIT        => $S_EXIT,
     )
 );
 
 $BUILDER->define_input(
-    $I_ALRM => (
-        $S_LOAD     => $S_RUN,
-        $S_RUN      => $S_RUN,
-        $S_SLEEP    => $S_RUN,
-        $S_GRACE    => $S_GRACE,
-        $S_SHUTDOWN => $S_EXIT,
-        $S_EXIT     => $S_EXIT,
+    $I_DONE => (
+        $S_LOAD        => $S_RUN,
+        $S_RUN         => $S_SLEEP,
+        $S_REAP        => $S_SLEEP,
+        $S_WATCH       => $S_SLEEP,
+        $S_SLEEP       => $S_SLEEP,
+        $S_REAP_GRACE  => $S_SLEEP_GRACE,
+        $S_WATCH_GRACE => $S_SLEEP_GRACE,
+        $S_SLEEP_GRACE => $S_EXIT,
+        $S_SHUTDOWN    => $S_EXIT,
+        $S_EXIT        => $S_EXIT,
+    )
+);
+
+$BUILDER->define_input(
+    $I_CHLD => (
+        $S_LOAD        => $S_REAP,
+        $S_RUN         => $S_REAP,
+        $S_REAP        => $S_REAP,
+        $S_WATCH       => $S_REAP,
+        $S_SLEEP       => $S_REAP,
+        $S_REAP_GRACE  => $S_REAP_GRACE,
+        $S_WATCH_GRACE => $S_REAP_GRACE,
+        $S_SLEEP_GRACE => $S_REAP_GRACE,
+        $S_SHUTDOWN    => $S_SHUTDOWN,
+        $S_EXIT        => $S_EXIT,
+    )
+);
+
+$BUILDER->define_input(
+    $I_USR1 => (
+        $S_LOAD        => $S_RUN,
+        $S_RUN         => $S_RUN,
+        $S_REAP        => $S_RUN,
+        $S_WATCH       => $S_RUN,
+        $S_SLEEP       => $S_RUN,
+        $S_REAP_GRACE  => $S_SLEEP_GRACE,
+        $S_WATCH_GRACE => $S_SLEEP_GRACE,
+        $S_SLEEP_GRACE => $S_SLEEP_GRACE,
+        $S_SHUTDOWN    => $S_EXIT,
+        $S_EXIT        => $S_EXIT,
     )
 );
 
 $BUILDER->define_input(
     $I_HUP => (
-        $S_LOAD     => $S_LOAD,
-        $S_RUN      => $S_LOAD,
-        $S_SLEEP    => $S_LOAD,
-        $S_GRACE    => $S_GRACE,
-        $S_SHUTDOWN => $S_EXIT,
-        $S_EXIT     => $S_EXIT,
+        $S_LOAD        => $S_LOAD,
+        $S_RUN         => $S_LOAD,
+        $S_REAP        => $S_LOAD,
+        $S_WATCH       => $S_LOAD,
+        $S_SLEEP       => $S_LOAD,
+        $S_REAP_GRACE  => $S_SLEEP_GRACE,
+        $S_WATCH_GRACE => $S_SLEEP_GRACE,
+        $S_SLEEP_GRACE => $S_SLEEP_GRACE,
+        $S_SHUTDOWN    => $S_EXIT,
+        $S_EXIT        => $S_EXIT,
     )
 );
 
 $BUILDER->define_input(
     $I_TERM => (
-        $S_LOAD     => $S_GRACE,
-        $S_RUN      => $S_GRACE,
-        $S_SLEEP    => $S_GRACE,
-        $S_GRACE    => $S_SHUTDOWN,
-        $S_SHUTDOWN => $S_EXIT,
-        $S_EXIT     => $S_EXIT,
+        $S_LOAD        => $S_SLEEP_GRACE,
+        $S_RUN         => $S_SLEEP_GRACE,
+        $S_REAP        => $S_SLEEP_GRACE,
+        $S_WATCH       => $S_SLEEP_GRACE,
+        $S_SLEEP       => $S_SLEEP_GRACE,
+        $S_REAP_GRACE  => $S_SHUTDOWN,
+        $S_WATCH_GRACE => $S_SHUTDOWN,
+        $S_SLEEP_GRACE => $S_SHUTDOWN,
+        $S_SHUTDOWN    => $S_EXIT,
+        $S_EXIT        => $S_EXIT,
     )
 );
 
 $BUILDER->define_input(
     $I_EXIT => (
-        $S_LOAD     => $S_SHUTDOWN,
-        $S_RUN,     => $S_SHUTDOWN,
-        $S_SLEEP,   => $S_SHUTDOWN,
-        $S_GRACE    => $S_SHUTDOWN,
-        $S_SHUTDOWN => $S_EXIT,
-        $S_EXIT     => $S_EXIT,
+        $S_LOAD        => $S_SHUTDOWN,
+        $S_RUN,        => $S_SHUTDOWN,
+        $S_REAP,       => $S_SHUTDOWN,
+        $S_WATCH       => $S_SHUTDOWN,
+        $S_SLEEP,      => $S_SHUTDOWN,
+        $S_REAP_GRACE  => $S_SHUTDOWN,
+        $S_WATCH_GRACE => $S_SHUTDOWN,
+        $S_SLEEP_GRACE => $S_SHUTDOWN,
+        $S_SHUTDOWN    => $S_EXIT,
+        $S_EXIT        => $S_EXIT,
     )
 );
 
--- server.pl
+++ server.pl
@@ -5,18 +5,12 @@ use feature 'say';
 
 use Readonly;
 use Heap::Binary;
-use ServerFSM qw( new_server_fsm $S_LOAD $S_RUN $S_SLEEP $S_GRACE $S_SHUTDOWN $S_EXIT $I_DEFAULT $I_ALRM $I_HUP $I_TERM $I_EXIT );
+use POSIX qw( pause );
+use ServerFSM qw( new_server_fsm %INPUT_PRIORITIES $S_LOAD $S_RUN $S_REAP $S_WATCH $S_SLEEP $S_REAP_GRACE $S_WATCH_GRACE $S_SLEEP_GRACE $S_SHUTDOWN $S_EXIT $I_ZERO $I_DONE $I_CHLD $I_ALRM $I_HUP $I_TERM $I_EXIT );
 
 my $CAUGHT_SIGALRM = 0;
 my $CAUGHT_SIGHUP  = 0;
 my $CAUGHT_SIGTERM = 0;
-
-Readonly my %PRIORITIES => (
-    $I_EXIT    => 0,
-    $I_TERM    => 1,
-    $I_HUP     => 2,
-    $I_DEFAULT => 3,
-);
 
 sub catch_sigalrm {
     $SIG{ALRM} = \&catch_sigalrm;
@@ -37,7 +31,7 @@ sub catch_sigterm {
 }
 
 my $input_queue =
-  Heap::Binary->new( sub { $PRIORITIES{ $_[0] } <=> $PRIORITIES{ $_[1] } } );
+  Heap::Binary->new( sub { $INPUT_PRIORITIES{ $_[0] } <=> $INPUT_PRIORITIES{ $_[1] } } );
 
 my %actions = (
     $S_LOAD => sub {
@@ -46,15 +40,31 @@ my %actions = (
     },
     $S_RUN => sub {
         say "Running";
+        return $I_DONE;
+    },
+    $S_REAP => sub {
+        say "Reaping";
+        return;
+    },
+    $S_WATCH => sub {
+        say "Watching";
         return;
     },
     $S_SLEEP => sub {
         say "Sleeping";
-        sleep;
+        pause;
         return;
     },
-    $S_GRACE => sub {
-        say "Shutting down gracefully";
+    $S_REAP_GRACE => sub {
+        say "Reaping during gracefull shutdown";
+        return;
+    },
+    $S_WATCH_GRACE => sub {
+        say "Watching during gracefull shutdown";
+        return;
+    },
+    $S_SLEEP_GRACE => sub {
+        say "Sleeping during gracefull shutdown";
         return;
     },
     $S_SHUTDOWN => sub {
@@ -88,6 +98,6 @@ while ($fsm->current ne $S_EXIT) {
         $input_queue->insert( $I_TERM );
     }
 
-    my $input = $input_queue->extract_min() // $I_DEFAULT;
+    my $input = $input_queue->extract_min() // $I_ZERO;
     $fsm->process( $input );
 }
